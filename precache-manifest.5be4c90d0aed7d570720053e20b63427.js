5fd9edb40d278d9f4646a901463c3d806b88cb38
Diefferson Koderer Môro <dieffersonmoro@gmail.com>
2019-10-31 22:24:47 +0000

    Updates

---
+++ precache-manifest.5be4c90d0aed7d570720053e20b63427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e709a1c175bd0fcb1d78a9c633be974f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bc72a81ce4059d9f8dc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "070d6a587e3f298c648e",
+    "url": "/static/js/2.e6a0a895.chunk.js"
+  },
+  {
+    "revision": "0bc72a81ce4059d9f8dc",
+    "url": "/static/js/main.dcdf524b.chunk.js"
+  },
+  {
+    "revision": "e47748b0bda1d3f99f78",
+    "url": "/static/js/runtime-main.c7af3e11.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
